e66ea503e758b3806404e8754d555544a8078b71
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2020-11-18 11:24:26 +0900

    Update newest Xamarin.Forms
    also add prism library

--- ITStudySearch/ITStudySearch/ITStudySearch/Views/DetailPageCS.cs
+++ ITStudySearch/ITStudySearch/ITStudySearch/Views/DetailPageCS.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 
 using Xamarin.Forms;
 using ITStudySearch.Models;
 using ITStudySearch.Converters;
+using Xamarin.Essentials;
 
 namespace ITStudySearch.Views
 {
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# IT 勉強会検索アプリ
+
+
+
+
+
+
+## 2020/11/18
+
+.NET Framework から .Net Core への移行
+
+[\.NET Framework から \.NET Core への移植 \- \.NET Core \| Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/core/porting/)
+
+
+
+
